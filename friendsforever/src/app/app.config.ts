831f4baa3265055e70bd7d55a637fd81edf4addb
Deepak28102000 <deepakrajsandeep@gmail.com>
2024-03-11 18:56:14 +0530

    init

--- friendsforever/src/app/app.config.ts
+++ friendsforever/src/app/app.config.ts
@@ -6,4 +6,6 @@ import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration()]
+
+  
 };
