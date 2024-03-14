0cd88816d6fab6994ff66c64b66c2a674fd7dd93
Deepak28102000 <deepakrajsandeep@gmail.com>
2024-03-14 18:33:29 +0530

    init

--- friendsforever/src/app/app.component.ts
+++ friendsforever/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {  RouterLink, RouterLinkActive, RouterOutlet  } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,4 +12,5 @@ import {  RouterLink, RouterLinkActive, RouterOutlet  } from '@angular/router';
 })
 export class AppComponent {
   title = 'friendsforever';
+  
 }
