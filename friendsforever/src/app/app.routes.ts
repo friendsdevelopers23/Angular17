ab8f135e59c7eaa5348894a6f6cb17450f41cdcc
Deepak28102000 <deepakrajsandeep@gmail.com>
2024-03-15 18:59:04 +0530

    init

--- friendsforever/src/app/app.routes.ts
+++ friendsforever/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {FirstComponent} from './first/first.component';
 import {SecondComponent} from './second/second.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+  { path: 'home-component', component: HomeComponent },
     { path: 'first-component', component: FirstComponent },
   { path: 'second-component', component: SecondComponent },
    { path: '',   redirectTo: '/first-component', pathMatch: 'full' },
