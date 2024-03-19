d835d39b80bd67ddb054a989010b612e6777aff5
Deepak28102000 <deepakrajsandeep@gmail.com>
2024-03-19 16:36:43 +0530

    init

--- friendsforever/src/app/app.component.ts
+++ friendsforever/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {  RouterLink, RouterLinkActive, RouterOutlet  } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HomeComponent]
 })
 export class AppComponent {
   title = 'friendsforever';
